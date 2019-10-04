702e33769c33e5365bb611690923109d75396668
MAC <mac@192.168.1.3>
2019-10-04 20:55:31 +0300

    minor changes

--- journal.py
+++ journal.py
@@ -1,13 +1,1 @@
 from app import app, db
-from app.models import User, Tutor, Group, Subject, Student
-
-@app.shell_context_processor
-def make_shell_contex():
-	return {
-			'db': db, 
-			'User': User, 
-			'Tutor': Tutor, 
-			'Group': Group,
-			'Subject': Subject,
-			'Student': Student,
-		}
