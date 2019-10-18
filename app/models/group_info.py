da3cd095aa2bbf12c7cede7c1a39a324c50a2771
Andrey <black_fox_0@mail.ru>
2019-10-18 22:14:10 +0300

    delete comments

--- app/models/group_info.py
+++ app/models/group_info.py
@@ -9,7 +9,3 @@ class GroupInfo(BaseModel):
     tgs_id = db.Column(db.Integer, db.ForeignKey('tgs.id'), nullable=False)
     dates = db.relationship('DatesInfo', lazy='dynamic',
         backref=db.backref('group_info', lazy=True), passive_deletes=True)
-    #name = db.Column(db.String(40), nullable=False)
-    #subject_name = db.Column(db.String(128), db.ForeignKey('subjects.name'), nullable=False)
-    #fields = db.relationship('CheckpointField', lazy='dynamic',
-    #    backref=db.backref('checkpoint', lazy=True), passive_deletes=True) 
